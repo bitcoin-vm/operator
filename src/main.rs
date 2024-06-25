340d0b4e0cc00196cc8fbb68cd8333cb68d8fae3
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-06-25 14:54:09 +0300

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest::Error;
+
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::thread;
 use std::time::Duration;
@@ -10,35 +10,35 @@ fn main() {
     dynamic_ip(0);
 }
 
-// A BVM operator can run on any instance with dynamic IP addresses.
+// A BVM operator can be run on any instance with dynamic IP addresses.
 // The operator regularly broadcasts their dynamic IP address from a dedicated Nostr account.
-// Clients can retrieve the operator's IP address from the latest Nostr post by the operator.
+// Clients can retrieve the operator's IP address from the the operator's latest Nostr post.
 // Nostr relays kind of act like DNS seeds
 
 fn dynamic_ip(mut counter: u32) {
-    thread::sleep(Duration::from_secs(10));
-
-    let ip_result: Result<String, Error> = return_ip();
+    let ip_result: Result<String> = return_ip();
 
     println!("counter: {}", counter);
 
     match ip_result {
         Ok(rst) => {
             println!("your dynamic ip address : {}", rst);
-            post_dynamic_ip(&rst);
+
+            try_post(&rst);
             counter = (counter + 1) % MODNUM;
         }
         Err(err) => {
-            println!("ip err, reason: {}", err);
+            println!("ip err: {}", err);
             counter = (counter + 1) % MODNUM;
         }
     }
 
+    thread::sleep(Duration::from_secs(10));
     dynamic_ip(counter)
 }
 
 #[tokio::main]
-async fn return_ip() -> Result<String, Error> {
+async fn return_ip() -> Result<String> {
     let url = "https://api.ipify.org?format=json";
 
     let response = reqwest::get(url).await?;
@@ -47,12 +47,19 @@ async fn return_ip() -> Result<String, Error> {
     if let Some(ip) = json["ip"].as_str() {
         Ok(ip.to_string())
     } else {
-        Ok(String::from(""))
+        Err("Error message".into())
     }
 }
 
+fn try_post(ip_str: &str) {
+    if let Err(_err) = post(ip_str) {
+        thread::sleep(Duration::from_secs(10));
+        try_post(ip_str)
+    } 
+}
+
 #[tokio::main]
-async fn post_dynamic_ip(ip_str: &str) -> Result<()> {
+async fn post(ip_str: &str) -> Result<()> {
 
     // Dedicated npub: npub1s9qann6psxk7ycnafsxtcak9zy74flaflre03855n0zqn9uhhvusva958z
     let my_keys = Keys::parse("6406f0f588179677e3d782a434153bc6dab57c529be669626603b28b2d0d96dc")?;
@@ -83,3 +90,4 @@ async fn post_dynamic_ip(ip_str: &str) -> Result<()> {
 
     Ok(())
 }
+
