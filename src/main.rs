c9a72587fa77df73e1eaf448a8178cb7d8b8bde2
brqgoo <100725376+brqgoo@users.noreply.github.com>
2024-06-25 17:50:16 +0300

    post only when the dynamic ip changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
-
+use nostr_sdk::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::ErrorKind;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::thread;
 use std::time::Duration;
-use nostr_sdk::prelude::*;
 
 const MODNUM: u32 = u32::MAX;
 
 fn main() {
+    let file = match File::open("src/ip.txt") {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("src/ip.txt") {
+                Ok(file) => file,
+                Err(_error) => panic!("could not create ip.txt"),
+            },
+            _ => panic!("unknown ip .txt error"),
+        },
+    };
+    drop(file);
+
+    println!("prev ip was: {}", retrieve_prev_ip());
+
     dynamic_ip(0);
+}
+
+fn retrieve_prev_ip() -> String {
+    let mut file = File::open("src/ip.txt").unwrap();
+
+    let mut ip_txt = String::new();
+    file.read_to_string(&mut ip_txt).unwrap();
+
+    ip_txt
 }
 
 // A BVM operator can be run on any instance with dynamic IP addresses.
@@ -16,24 +41,23 @@ fn main() {
 // Nostr relays kind of act like DNS seeds
 
 fn dynamic_ip(mut counter: u32) {
-    let ip_result: Result<String> = return_ip();
-
     println!("counter: {}", counter);
-
+    let ip_result: Result<String> = return_ip();
     match ip_result {
         Ok(rst) => {
-            println!("your dynamic ip address : {}", rst);
+            if retrieve_prev_ip() != rst {
+                println!("new ip detected: {}", rst);
+                try_post(&rst);
+                println!("new ip posted: {}", rst);
 
-            try_post(&rst);
-            counter = (counter + 1) % MODNUM;
+                let mut file = File::create("src/ip.txt").unwrap();
+                file.write_all(rst.as_bytes()).unwrap();
+            }
         }
-        Err(err) => {
-            println!("ip err: {}", err);
-            counter = (counter + 1) % MODNUM;
-        }
+        Err(_err) => println!("ip address could not be retrieved.")
     }
-
-    thread::sleep(Duration::from_secs(10));
+    counter = (counter + 1) % MODNUM;
+    thread::sleep(Duration::from_secs(5));
     dynamic_ip(counter)
 }
 
@@ -53,16 +77,15 @@ async fn return_ip() -> Result<String> {
 
 fn try_post(ip_str: &str) {
     if let Err(_err) = post(ip_str) {
-        thread::sleep(Duration::from_secs(10));
+        thread::sleep(Duration::from_secs(1));
         try_post(ip_str)
-    } 
+    }
 }
 
 #[tokio::main]
-async fn post(ip_str: &str) -> Result<()> {
-
-    // Dedicated npub: npub1s9qann6psxk7ycnafsxtcak9zy74flaflre03855n0zqn9uhhvusva958z
-    let my_keys = Keys::parse("6406f0f588179677e3d782a434153bc6dab57c529be669626603b28b2d0d96dc")?;
+async fn post(ip_str: &str) -> Result<String> {
+    // Dedicated npub: npub15jmehrv33xzw5xgt2khp50daxgzvp7v0zrrwmcdzhvsjkz8h7lrsh8ygxq
+    let my_keys = Keys::parse("0d30b892172b3b7af48c19224f577e8a94a49949ec65bdfbb8d18f455dd6d591")?;
 
     let client = Client::new(&my_keys);
 
@@ -74,20 +97,12 @@ async fn post(ip_str: &str) -> Result<()> {
             "wss://relay.nostr.info",
             RelayOptions::new().proxy(proxy).write(false),
         )
-        .await?;
-    client
-        .add_relay_with_opts(
-            "ws://jgqaglhautb4k6e6i2g34jakxiemqp6z4wynlirltuukgkft2xuglmqd.onion",
-            RelayOptions::new().proxy(proxy),
-        )
-        .await?;
+        .await.unwrap();
 
     client.connect().await;
 
-    client
-        .publish_text_note(ip_str.to_string(), [])
-        .await?;
-
-    Ok(())
+    match client.publish_text_note(ip_str.to_string(), []).await {
+        Ok(eventid) => Ok(eventid.to_string()),
+        Err(_error) => Err("post error".into()),
+    }
 }
-
